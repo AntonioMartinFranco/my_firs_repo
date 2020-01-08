70965cbc1c7ae4ace56a7de154863e7eff2642bc
Antonio Franco <Francoa.soporte@gmail.com>
2020-01-08 16:20:56 -0300

    1.00

--- Costo.py
+++ Costo.py
@@ -1,12 +1,7 @@
 from colorama import Style, init
 init()
-COMICION = 0.05
-IVA = 0.21
+CARGOS = [0.05, 0.21]
 NG = Style.BRIGHT
-
-
-def porcentaje(neto, porciento):
-    return(neto*porciento)/100
 
 
 def porcen(neto, porciento):
@@ -14,16 +9,16 @@ def porcen(neto, porciento):
 
 
 def Valor_con_env_incliedo2(monto_enviar):
-    cargos = porcen(monto_enviar, COMICION)
-    cargos_iva = (float(porcen(cargos, IVA)))
+    cargos = porcen(monto_enviar, CARGOS[0])
+    cargos_iva = porcen(cargos, CARGOS[1])
     return monto_enviar, cargos, cargos_iva
 
 
-print(NG + "|WESTERN UNION|")
+print(NG + "PROGRAMA PARA CALCULAR EL COSTO DE ENVIO ")
 print(NG + "Costos de envio")
 print(NG + "Por Favor Ingrese el monto que desea enviar...")
 mont = float(input("$ "))
 lista = []
 lista.extend(Valor_con_env_incliedo2(mont))
-print(NG + f"Usted desea enviar ${lista[0]} y tiene un costo de ${lista[1]}+ el iva de ${lista[2]}")
+print(NG + f"Usted desea enviar ${lista[0]} y tiene un costo de ${lista[1]} + el iva de ${lista[2]}")
 print(NG + f"en Total ${round(lista[0]+lista[1]+lista[2],2)}")
