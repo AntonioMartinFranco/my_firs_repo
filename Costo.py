b3b1ef1e05239fc0baa829d52a196fe3244472e8
Antonio Franco <Francoa.soporte@gmail.com>
2020-01-08 16:25:14 -0300

    a

--- Costo.py
+++ Costo.py
@@ -4,14 +4,10 @@ CARGOS = [0.05, 0.21]
 NG = Style.BRIGHT
 
 
-def porcen(neto, porciento):
-    return(neto * porciento)
-
-
 def Valor_con_env_incliedo2(monto_enviar):
-    cargos = porcen(monto_enviar, CARGOS[0])
-    cargos_iva = porcen(cargos, CARGOS[1])
-    return monto_enviar, cargos, cargos_iva
+    cargos = monto_enviar*CARGOS[0]
+    cargos_iva = cargos * CARGOS[1]
+    return cargos, cargos_iva
 
 
 print(NG + "PROGRAMA PARA CALCULAR EL COSTO DE ENVIO ")
@@ -20,5 +16,5 @@ print(NG + "Por Favor Ingrese el monto que desea enviar...")
 mont = float(input("$ "))
 lista = []
 lista.extend(Valor_con_env_incliedo2(mont))
-print(NG + f"Usted desea enviar ${lista[0]} y tiene un costo de ${lista[1]} + el iva de ${lista[2]}")
-print(NG + f"en Total ${round(lista[0]+lista[1]+lista[2],2)}")
+print(NG + f"Usted desea enviar ${mont} y tiene un costo de ${lista[0]} + el iva de ${lista[1]}")
+print(NG + f"en Total ${round(mont +lista[0]+lista[1],3)}")
